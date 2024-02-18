321b906c2f07c123d80d104b0ce9faa37ba3ac7f
HN026 <plancksconstant01@gmail.com>
2024-02-18 15:49:37 +0530

    colored

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import pygame
 from pygame.locals import *
 
-def subdivide(surface, p1, p2, p3, level):
-    if level >= 9:
+def subdivide(v, p, q, r, col, level):
+    if level >= 10:
+        pixel_col = (min(col[0], 255), min(col[1], 255), min(col[2], 255))
+        pygame.draw.polygon(surface, pixel_col, ((int(p.pos[0]), int(p.pos[1])),
+                                                 (int(q.pos[0]), int(q.pos[1])),
+                                                 (int(r.pos[0]), int(r.pos[1]))))
         return
 
-    pygame.draw.lines(surface, (255, 255, 255), True, (p1, p2, p3))
-    pygame.display.update()
+    col = (randint(col[0], col[0] + 6), randint(col[1], col[1] + 6), randint(col[2], col[2] + 6))
+    m = Point(((p.pos[0] + q.pos[0]) / 2, (p.pos[1] + q.pos[1]) / 2), col)
+    n = Point(((q.pos[0] + r.pos[0]) / 2, (q.pos[1] + r.pos[1]) / 2), col)
+    o = Point(((p.pos[0] + r.pos[0]) / 2, (p.pos[1] + r.pos[1]) / 2), col)
 
-    subdivide(surface, p1, ((p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2), ((p1[0] + p3[0]) / 2, (p1[1] + p3[1]) / 2), level + 1)
-    subdivide(surface, p2, ((p2[0] + p3[0]) / 2, (p2[1] + p3[1]) / 2), ((p2[0] + p1[0]) / 2, (p2[1] + p1[1]) / 2), level + 1)
-    subdivide(surface, p3, ((p3[0] + p2[0]) / 2, (p3[1] + p2[1]) / 2), ((p3[0] + p1[0]) / 2, (p3[1] + p1[1]) / 2), level + 1)
+    subdivide(v, m, n, o, p.col if v[0] == 0 else col, level + 1)
+    subdivide(v, p, o, m, p.col if v[1] == 0 else col, level + 1)
+    subdivide(v, m, n, q, p.col if v[2] == 0 else col, level + 1)
+    subdivide(v, o, r, n, p.col if v[3] == 0 else col, level + 1)
 
+    
 def main():
     pygame.init()
     surface = pygame.display.set_mode((800, 600))
